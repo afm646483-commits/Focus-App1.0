7e5fd394781e05d435f65688b707e068771f371c
andres <castillosotoandresfelipe@gmail.com>
2025-11-08 18:12:13 -0500

    push master2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,12 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    // Jetpack Compose Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-// Jetpack Compose (por si no lo tienes aún)
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.compose.ui:ui:1.6.8")
-    implementation("androidx.compose.material3:material3:1.2.1")
-// Kotlin Coroutines (requerido para 'delay')
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
